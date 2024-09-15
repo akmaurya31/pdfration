7e701e2a2371f67f6edc7e7ea2930b3d4d7e67ea
aqadavinash <avinash@aqad.ae>
2024-09-15 14:50:37 +0530

    add

--- header.php
+++ header.php
@@ -96,9 +96,12 @@ function getCurWallet($mysqli,$userId) {
         <a href="pay.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">  💰Recharge</a>
       </li>
 
+
+      <?php if($_SESSION['role']=='admin') { ?>
       <li>
         <a href="userList.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">  📋User List</a>
       </li>
+      <?php } ?>
 
       
       <li>
