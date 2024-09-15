829f43472855d662762065a373e7dcc3bde8720f
aqadavinash <avinash@aqad.ae>
2024-09-15 12:52:35 +0530

    add

--- dbconn.php
+++ dbconn.php
@@ -1,11 +1,11 @@
 <?php 
-// $databaseHost = 'mydb.cj2aycaoyu63.us-west-1.rds.amazonaws.com';
-// $databaseName = 'cardration';
-// $databaseUsername = 'admin';
-// $databasePassword = 'Luxyaragroup';
-
-$databaseHost = 'localhost';
+$databaseHost = 'mydb.cj2aycaoyu63.us-west-1.rds.amazonaws.com';
 $databaseName = 'cardration';
-$databaseUsername = 'root';
-$databasePassword = 'root@123';
+$databaseUsername = 'admin';
+$databasePassword = 'Luxyaragroup';
+
+// $databaseHost = 'localhost';
+// $databaseName = 'cardration';
+// $databaseUsername = 'root';
+// $databasePassword = 'root@123';
 ?>
