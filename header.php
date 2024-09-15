f90a22cdc3926845ad7ad199b801bd5d5fbe14c2
aqadavinash <avinash@aqad.ae>
2024-09-15 14:51:53 +0530

    add

--- header.php
+++ header.php
@@ -105,7 +105,7 @@ function getCurWallet($mysqli,$userId) {
 
       
       <li>
-        <a href="wallet.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">  👛Wallet</a>
+        <a href="wallet.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">  👛Wallet ₹ [<?php echo $_SESSION['current_balance']; ?>]</a>
       </li>
 
       <li>
