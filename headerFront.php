1bfd342844105d397ba80b8a4d37ead1b35a222e
aqadavinash <avinash@aqad.ae>
2024-09-21 06:42:45 +0530

    add

--- headerFront.php
+++ headerFront.php
@@ -28,27 +28,24 @@
         </div>
     </nav>
     <nav class="bg-gray-50">
-        <div class="max-w-screen-xl px-4 py-3 mx-auto">
-            <div class="flex items-center">
-                <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+         <div class="max-w-screen-xl px-4 py-3 mx-auto overflow-x-auto ">
+            <div class="flex items-center my-2">
+                <ul class="flex flex-row font-medium mt-0 space-x-4 rtl:space-x-reverse text-sm">
                     
                     <li>
-                        <a href="otherservices.php" class="text-gray-900  hover:underline">🛠️OtherSerivces</a>
+                        <a href="otherservices.php" class="text-gray-900 p-2 rounded hover:underline whitespace-nowrap border border-blue-400 ">🛠️OtherSerivces</a>
                     </li>
                     <li>
-                        <a href="partnership.php" class="text-gray-900  hover:underline">🤝Partnership</a>
+                        <a href="partnership.php" class="text-gray-900 p-2 rounded  hover:underline whitespace-nowrap border border-red-400 ">🤝Partnership</a>
                     </li>
                     <li>
-                        <a href="personalmeet.php" class="text-gray-900  hover:underline">🗣️ PersonalMeet</a>
+                        <a href="personalmeet.php" class="text-gray-900 p-2 rounded  hover:underline whitespace-nowrap border border-yellow-400 ">🗣️ PersonalMeet</a>
                     </li>
 
                     <li>
-                    <a type="button" href="festivaloffer.php" class="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">🎉Festival Offer</a>
+                    <a type="button" href="festivaloffer.php" class="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center whitespace-nowrap">🎉Festival Offer</a>
                     </li>
                 </ul>
             </div>
         </div>
-
-  
-
     </nav>
