9b197ac8179b0a811b96e47a7cca09419ea97090
aqadavinash <avinash@aqad.ae>
2024-10-04 00:51:18 +0530

    add

--- webtgep/dbconn.php
+++ webtgep/dbconn.php
@@ -1,12 +1,12 @@
 <?php 
-// $databaseHost = 'mydb.cj2aycaoyu63.us-west-1.rds.amazonaws.com';
-// $databaseName = 'cardration';
-// $databaseUsername = 'admin';
-// $databasePassword = 'Luxyaragroup';
-
-
-$databaseHost = 'localhost';
+$databaseHost = 'mydb.cj2aycaoyu63.us-west-1.rds.amazonaws.com';
 $databaseName = 'tpeg';
-$databaseUsername = 'root';
-$databasePassword = 'root@123';
+$databaseUsername = 'admin';
+$databasePassword = 'Luxyaragroup';
+
+
+// $databaseHost = 'localhost';
+// $databaseName = 'tpeg';
+// $databaseUsername = 'root';
+// $databasePassword = 'root@123';
 ?>
