7be1020c3139a413540b7acffc8e9efe3bd1a5fc
aqadavinash <avinash@aqad.ae>
2024-12-11 14:01:28 +0530

    add

--- jqeditRation.php
+++ jqeditRation.php
@@ -5,12 +5,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $mukhiya = $_POST['mukhiya']; 
     $janpad = $_POST['Janpad'];
     $DrivePath = $_POST['DrivePath'];
+    $DrivePath1=convertToDownloadLink($DrivePath);
+
     $rid = $_POST['rid'];
        $sql = "UPDATE ration_req SET 
         ration='$ration_no', 
         name='$mukhiya', 
         janpad='$janpad',
-        pdf_path='$DrivePath'
+        pdf_path='$DrivePath1'
         WHERE id=$rid";
     if ($mysqli->query($sql) === TRUE) {
         echo "User information updated successfully.";
@@ -19,4 +21,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 $mysqli->close();
+ 
+// Function to convert Google Drive view link to download link
+function convertToDownloadLink($viewLink) {
+    // Parse the URL and extract the file ID
+    $parsedUrl = parse_url($viewLink);
+    parse_str($parsedUrl['query'], $queryParams);
+
+    // Check if 'id' is available in the query params, else extract from path
+    if (isset($queryParams['id'])) {
+        $fileId = $queryParams['id'];
+    } else {
+        // Extract the file ID from the URL path
+        $pathSegments = explode('/', $parsedUrl['path']);
+        $fileId = end($pathSegments);
+    }
+
+    // Return the formatted download link
+    return "https://drive.usercontent.google.com/u/0/uc?id=" . $fileId . "&export=download";
+}
+
+// // Example usage
+// $inputLink = "https://drive.google.com/file/d/1ELDQq5JLIpGkbkoDjEskIFD73UyUPbAE/view?usp=sharing";
+// $downloadLink = convertToDownloadLink($inputLink);
+
+// echo "Download Link: " . $downloadLink;
 ?>
